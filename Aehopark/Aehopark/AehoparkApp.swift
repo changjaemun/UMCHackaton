0c98a554309b241d1023f2dbab49563963b99200
문창재 <ckdwo23@gmail.com>
2024-06-28 18:47:27 +0900

    애호박 추가

---
+++ Aehopark/Aehopark/AehoparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AehoparkApp.swift
+//  Aehopark
+//
+//  Created by 문창재 on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct AehoparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aehopark/Aehopark/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aehopark
+//
+//  Created by 문창재 on 6/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
