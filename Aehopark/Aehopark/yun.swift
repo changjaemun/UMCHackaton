d7d9f7d409d6fd64d2ee1a502fa1f0380e6cdfe2
윤 <dbstjd1576@gmail.com>
2024-06-28 18:53:29 +0900

    yun 파일 추가

---
+++ Aehopark/Aehopark/yun.swift
@@ -0,0 +1,18 @@
+//
+//  yun.swift
+//  Aehopark
+//
+//  Created by 윤성 on 6/28/24.
+//
+
+import SwiftUI
+
+struct yun: View {
+    var body: some View {
+        Text("yun")
+    }
+}
+
+#Preview {
+    yun()
+}
